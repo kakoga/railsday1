42c244b0d92bcf85ae32d5d4311701fc46949601
daniel.kim <crimsondex@yahoo.com>
2016-02-17 13:29:53 -0800

    hilogame final

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,53 +8,51 @@ class MainController < ApplicationController
       # run the random number generator and assign it to cookie: secret number
       cookies[:secret_number] = rand(100)
     end
+    # assigns instance cookie: (integer) secret number to instance var secret number
     @secret_number = cookies[:secret_number].to_i
 
+    # checks to see if cookies: counter is empty...
     if cookies[:counter].nil?
-      # run the random number generator and assign it to cookie: secret number
+      # if it's empty then it sets cookies: counter to 5
       cookies[:counter] = 5
     end
+    # and assigns cookies: (integer) counter to instance var counter
     @counter = cookies[:counter].to_i
 
+    # checks to see if parameter guess taken from the url is empty
     if params[:guess].nil?
-      # Did not get a guess from browser/user
+      # just checks to see if it's empty but does not execute any code if it IS empty
     else
+      # if parameter guess taken from url is NOT empty, then it params:guess is assigned to instance var guess - as an integer
       @guess = params[:guess].to_i
 
+      # checks to see if instance var secret number  is equal to the instance var guess
       if @secret_number == @guess
+        # if above statement is true then assigns "You Won" to instance var result.
         @result = "You Won!"
+        # if above statement is true, then cookies: secret number is reset to another random number
         cookies[:secret_number] = rand(100)
+        # if above statement is true, then cookies:counter is reset to 5
         cookies[:counter] = 5
-      else
+
+        # if if-statement is false, then the counter counts down by 1
+      elsif @counter > 0
         @counter = @counter - 1
+        # and re-assigns instance var counter to cookies:counter.
+        # needs reassignment because the counter minus one equals 4, and in order to keep counting down it needs to be reassigned to cookies: counter. otherwise it will always start at 5.
         cookies[:counter] = @counter
-        @result = @guess.to_s + " is wrong!" + " you only have " + @counter.to_s + " guesses left."
+        # if if-statement is false then tells the user that their guess was wrong and that they have x more tries left to guess the secret number
+        if cookies[:counter].to_i == 0
+          @result = "You lost, enter a new guess to start again"
+          cookies[:secret_number] = rand(100)
+          cookies[:counter] = 5
+
+        else
+          @result = @guess.to_s + " is wrong!" + " you only have " + @counter.to_s + " guesses left."
+        end
+
 
       end
-
     end
-    # # if cookie: secret number is empty, then
-    # if(cookies[:secret_number].nil?)
-    #   # run the random number generator and assign it to cookie: secret number
-    #   cookies[:secret_number] = rand(100)
-    #   # else if the cookie: secret number is equal to the guess then tell the user that they won.
-    #   cookies[:number] = 5
-    # elsif @secret_number == params[:guess].to_i && @counter > 0
-    #   @result = "you won!"
-    #   # if the user has won, then generate a new random number
-    #   cookies[:secret_number] = rand(100)
-    #   # if the cookie: secret number is lower than the guess, alert the user: too high!
-    #   cookies[:number] = 5
-    # elsif @secret_number < params[:guess].to_i && @counter > 0
-    #   @result = "too high!"
-    #   # if the cookie: secret number is higher than the guess, alert user: too low!
-    #   cookies[:number] -= 1
-    # elsif @secret_number > params[:guess].to_i && @counter > 0
-    #   @result = "too low!"
-    #   cookies[:number] -= 1
-    #
-    # end
-
   end
-
 end
